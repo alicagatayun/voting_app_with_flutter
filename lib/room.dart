d2bb742377005ed22adcde4f0e7e07b11b25e015
alicagatayun <acagatayu@gmail.com>
2023-02-15 10:20:56 +0300

    butonlar yerleştirildi

--- lib/room.dart
+++ lib/room.dart
@@ -46,49 +46,111 @@ class _RoomManagementState extends State<RoomManagement> {
                   columns;
 
           return Scaffold(
-            appBar: AppBar(
-              title: Text(docs['name']),
-            ),
-            body: SingleChildScrollView(
-              physics: const ScrollPhysics(),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(docs['vote_status']),
-                  ),
-                  Wrap(
-                    spacing: spacing, //vertical spacing
-                    runSpacing: runSpacing, //horizontal spacing
-                    alignment: WrapAlignment.spaceEvenly,
-                    children: List<Widget>.generate(data.length, (index) {
-                      final userData = data[index];
-                      return SizedBox(
-                        width: w,
-                        height: w,
+              appBar: AppBar(
+                title: Text(docs['name']),
+              ),
+              body: LayoutBuilder(
+                builder: (context, constraint) {
+                  return SingleChildScrollView(
+                    physics: const ScrollPhysics(),
+                    child: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(minHeight: constraint.maxHeight),
+                      child: IntrinsicHeight(
                         child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            CircleAvatar(
-                              child: Text(userData['id'].toString()),
-                            ),
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                docs['vote_status'] == "VOTING"
-                                    ? ("Voting")
-                                    : data[index][
-                                        'sp'], // Use the fullName property of each item
-                              ),
+                              padding: const EdgeInsets.all(24.0),
+                              child: Text(docs['vote_status']),
+                            ),
+                            Wrap(
+                              spacing: spacing, //vertical spacing
+                              runSpacing: runSpacing, //horizontal spacing
+                              alignment: WrapAlignment.spaceEvenly,
+                              children:
+                                  List<Widget>.generate(data.length, (index) {
+                                final userData = data[index];
+                                return SizedBox(
+                                  width: w,
+                                  height: w,
+                                  child: Column(
+                                    children: [
+                                      CircleAvatar(
+                                        child: Text(userData['id'].toString()),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          docs['vote_status'] == "VOTING"
+                                              ? ("Voting")
+                                              : data[index][
+                                                  'sp'], // Use the fullName property of each item
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }),
+                            ),
+                            const Spacer(),
+                            Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("0")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("1")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("2")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("3"))
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("5")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("8")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("13")),
+                                    ElevatedButton(
+                                        onPressed: () {}, child: Text("21"))
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 16.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      ElevatedButton(
+                                          onPressed: () {}, child: Text("34")),
+                                      ElevatedButton(
+                                          onPressed: () {}, child: Text("55")),
+                                      ElevatedButton(
+                                          onPressed: () {}, child: Text("89")),
+                                      ElevatedButton(
+                                          onPressed: () {}, child: Text("144"))
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      );
-                    }),
-                  ),
-                ],
-              ),
-            ),
-          );
+                      ),
+                    ),
+                  );
+                },
+              ));
         }
         return const Center(child: CircularProgressIndicator());
       },
